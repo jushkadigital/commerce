471afae5da6b8f375476dd9ab8d45c4cff4f1843
josh <urgosxd@gmail.com>
2026-01-29 16:31:47 -0500

    aoe

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,7 +1,8 @@
 import Medusa from "@medusajs/js-sdk"
 
+const BACKEND_URL = process.env.MEDUSA_BACKEND_URL || "https://commerce.patarutera.pe"
 export const sdk = new Medusa({
-  baseUrl: "http://localhost:9000",
+  baseUrl: BACKEND_URL,
   debug: true,
   auth: {
     type: "session",
