f7953a07b80839d240d6bd488797e0b7ced827ac
josue <urgosxd@gmail.com>
2026-06-12 02:24:35 -0500

    aoe

--- src/admin/lib/sdk.ts
+++ src/admin/lib/sdk.ts
@@ -1,11 +1,9 @@
 import Medusa from "@medusajs/js-sdk"
 
-let MEDUSA_BACKEND_URL = "http://localhost:9000"
+const MEDUSA_BACKEND_URL =
+  import.meta.env.VITE_MEDUSA_BACKEND_URL ||
+  (import.meta.env.PROD ? "https://commerce.patarutera.pe" : "http://localhost:9000")
 
-if (import.meta.env.VITE_MEDUSA_BACKEND_URL) {
-  MEDUSA_BACKEND_URL = import.meta.env.VITE_MEDUSA_BACKEND_URL
-}
-//const publicUrl = 'https://commerce.patarutera.pe'
 export const sdk = new Medusa({
   baseUrl: MEDUSA_BACKEND_URL,
   debug: false,
